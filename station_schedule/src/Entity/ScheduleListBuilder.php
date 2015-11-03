b57f2f2f4908730f29249e21ded35bb0e9004a1c
Tim Plunkett <git@plnktt.com>
2015-11-03 17:10:22 -0500

    Expand usefulness of schedule listing.

--- station_schedule/src/Entity/ScheduleListBuilder.php
+++ station_schedule/src/Entity/ScheduleListBuilder.php
@@ -33,6 +33,9 @@ class ScheduleListBuilder extends EntityListBuilder {
   public function buildHeader() {
     $header = [
       'title' => $this->t('Title'),
+      'item_count' => $this->t('Scheduled items'),
+      'days_empty' => $this->t('Days empty'),
+      'unfilled_time' => $this->t('Unfilled time'),
     ] + parent::buildHeader();
     return $header;
   }
@@ -41,11 +44,31 @@ class ScheduleListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
+    /** @var \Drupal\station_schedule\ScheduleInterface $entity */
     $row['title']['data'] = [
       '#type' => 'link',
       '#title' => $entity->label(),
       '#url' => $entity->urlInfo(),
     ];
+
+    $row['item_count'] = count($entity->getScheduledItems());
+
+    $days_in_week = 7;
+    $scheduled_items_by_day = $entity->getScheduledItemsByDay();
+    $row['days_empty'] = $days_in_week - count(array_filter($scheduled_items_by_day));
+
+    $hours_per_week = ($entity->getEndHour() - $entity->getStartHour()) * $days_in_week;
+    $duration = 0;
+    foreach ($scheduled_items_by_day as $schedule_items) {
+      /** @var \Drupal\station_schedule\ScheduleItemInterface[] $schedule_items */
+      foreach ($schedule_items as $schedule_item) {
+        $duration += $schedule_item->getFinish() - $schedule_item->getStart();
+      }
+    }
+    $filled_hours = $duration / 60;
+    $unfilled_hours = $hours_per_week - $filled_hours;
+    $row['unfilled_time'] = $this->t('@hours hours / @percentage%', ['@hours' => $unfilled_hours, '@percentage' => round($unfilled_hours * 100 / $hours_per_week)]);
+
     return $row + parent::buildRow($entity);
   }
 

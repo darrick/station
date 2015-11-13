39c3a8f93727771d2df944681425b322f4cb0005
Tim Plunkett <git@plnktt.com>
2015-11-13 15:59:51 -0500

    Handle when no scheduled item exists.

--- station_schedule/src/Plugin/Block/ScheduledItemBlock.php
+++ station_schedule/src/Plugin/Block/ScheduledItemBlock.php
@@ -10,6 +10,7 @@ namespace Drupal\station_schedule\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityViewBuilderInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\station_schedule\ScheduleRepositoryInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -19,11 +20,16 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "station_schedule_item",
  *   admin_label = @Translation("Scheduled item"),
  *   context = {
- *     "schedule item" = @ContextDefinition("entity:station_schedule_item")
+ *     "schedule item" = @ContextDefinition("entity:station_schedule_item", required = FALSE)
  *   }
  * )
  */
 class ScheduledItemBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\station_schedule\ScheduleRepositoryInterface
+   */
+  protected $scheduleRepository;
 
   /**
    * ScheduledItemBlock constructor.
@@ -31,10 +37,12 @@ class ScheduledItemBlock extends BlockBase implements ContainerFactoryPluginInte
    * @param array $configuration
    * @param string $plugin_id
    * @param mixed $plugin_definition
+   * @param \Drupal\station_schedule\ScheduleRepositoryInterface $schedule_repository
    * @param \Drupal\Core\Entity\EntityViewBuilderInterface $view_builder
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityViewBuilderInterface $view_builder) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ScheduleRepositoryInterface $schedule_repository, EntityViewBuilderInterface $view_builder) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->scheduleRepository = $schedule_repository;
     $this->scheduleItemViewBuilder = $view_builder;
   }
 
@@ -46,6 +54,7 @@ class ScheduledItemBlock extends BlockBase implements ContainerFactoryPluginInte
       $configuration,
       $plugin_id,
       $plugin_definition,
+      $container->get('station_schedule.schedule.repository'),
       $container->get('entity_type.manager')->getViewBuilder('station_schedule_item')
     );
   }
@@ -54,7 +63,18 @@ class ScheduledItemBlock extends BlockBase implements ContainerFactoryPluginInte
    * {@inheritdoc}
    */
   public function build() {
-    return $this->scheduleItemViewBuilder->view($this->getContextValue('schedule item'));
+    if ($item = $this->getContextValue('schedule item')) {
+      return $this->scheduleItemViewBuilder->view($item);
+    }
+    else {
+      return [
+        '#theme' => 'station_schedule_item_empty',
+        '#time' => $this->t('For now...'),
+        '#program' => $this->scheduleRepository->getCurrentSchedule()->getUnscheduledMessage(),
+        '#djs' => $this->t('Nobody'),
+        '#genre' => $this->t('Random music'),
+      ];
+    }
   }
 
 }
